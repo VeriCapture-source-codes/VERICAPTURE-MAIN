1ae1b015f8f02a539339f3106734b697d72d978c
Therook <ezeliorae@gmail.com>
2025-05-07 17:23:14 +0100

    update LandingPage

--- FRONT-END/src/LandingPage.jsx
+++ FRONT-END/src/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Landingpage.css';
+import './LandingPage.css';
 
 const Landingpage = () => {
   const navigate = useNavigate();
