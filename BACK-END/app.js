7e4dc1494ffe10f44af905739a1b6a81cb0eaffa
Therook <ezeliorae@gmail.com>
2025-05-14 17:37:08 +0100

    update deploy workflow

--- BACK-END/app.js
+++ BACK-END/app.js
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
+      "http://localhost:5000",
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "https://www.vericapture.com.ng",
